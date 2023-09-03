10247bcdcd93039fbdd938baa2ce31b82ee27434
Ivan Veluz <johnivanveluz@gmail.com>
2023-09-04 00:35:55 +0800

    homepage configuration

--- src/scenes/Home/index.tsx
+++ src/scenes/Home/index.tsx
@@ -1,4 +1,3 @@
-import useMediaQuery from "@/hooks/useMediaQuery";
 import { CurrentPage } from "@/shared/types"
 import { motion } from "framer-motion";
 import portrait_gray from "@/assets/portrait_gray.png"
@@ -8,13 +7,11 @@ type Props = {
 }
 
 const Home = ({setCurrentPage}: Props) => {
-  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
-
   return (
     <section id="home" className="gap-16 bg-primary-100 py-24 md:h-full md:pb-0">
-      <motion.div className="mx-auto w-4/6 items-center justify-center md:flex md:h-5/6"
+      <motion.div className="mx-auto w-4/6 items-center justify-center md:flex md:h-5/6 md:w-5/6"
       onViewportEnter={() => setCurrentPage(CurrentPage.Home)}>
-        <motion.div className="before:absolute before:-top-20 before:-left-20 before:z-[-1]"
+        <motion.div className=""
           initial="hidden"
           whileInView="visible"
           viewport={{once:true, amount: 0.5}} 
@@ -23,10 +20,10 @@ const Home = ({setCurrentPage}: Props) => {
             hidden: {opacity: 0, y:-50},
             visible: {opacity: 1, y: 0}
         }}>
-          <img className="rounded-full object-scale-down h-80" src={portrait_gray} alt="" />
+          <img className="h-80 rounded-full md:z-10 md:ml-40 md:justify-items-end" src={portrait_gray} alt="" />
         </motion.div>
-        <div className="z-10 mt-20 md:basis-3/5">
-          <motion.div className="md:-mt-20 ml-20"
+        <div className="z-10 mt-20 md:basis-4/6">
+          <motion.div className="md:-mt-20 md:ml-28"
             initial="hidden"
             whileInView="visible"
             viewport={{once:true, amount: 0.5}} 
@@ -42,11 +39,11 @@ const Home = ({setCurrentPage}: Props) => {
               </div>
             </div>
           </motion.div >
-          <div className="mx-auto w-4/6 items-center justify-center md:h-5/6">
+          <div className="mx-auto w-5/6 items-center justify-center md:h-5/6 md:w-4/6">
               <p className="mt-4 text-3xl items-center justify-center">
                 Fullstack Web Developer | Software Engineer | AI/ML
             </p>
-            <span className="mx-auto space-x-4 justify-center flex">
+            <span className="mx-auto space-x-4 flex items-center justify-center md:justify-normal">
               <button type="submit"
                       className="mt-5 rounded-full bg-secondary-500 px-8 py-3 transition duration-300 hover:text-white">Download CV</button>
               <button type="submit"
